de3f3112eb9cc7f4cf1a7801560bc1a46c6ccca2
fujispecial <takeabreakjp@gmail.com>
2020-09-11 11:09:41 +0900

    PAY.JPの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,9 +7,22 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
+
+  def order_params
+    params.require(:order).permit(:token, :item_id, :user_id, :item_price)
+  end
 
   def move_to_index
     unless user_signed_in?
@@ -24,4 +37,12 @@ class OrdersController < ApplicationController
     end
   end
 
+  def pay_item
+    Payjp.api_key = "sk_test_57ca6f9bfe5769a11edb9e58"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
 end
